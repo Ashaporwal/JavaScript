ebc4f9ce2bb80a4430020fe147349cb22ae84bab
ashap127 <porwalasha58@gmail.com>
2025-05-28 20:23:53 +0530

    Remove node_modules and fix line endings

---
+++ Nodejs/introToNode/cardModeule.js
@@ -0,0 +1,13 @@
+let addtocart = ()=>{
+    return "add to card"
+}
+
+let changeQ = ()=>{
+    return "add to card"
+}
+
+let a = "hello";
+
+// module.exports = addtocart
+
+module.exports = {addtocart,changeQ,a}
---
+++ Nodejs/introToNode/index.js
@@ -0,0 +1,18 @@
+const {addtocart, changeQ,a} = require("./cardModeule");
+
+console.log("WElcome to node js");
+console.log(10+90);
+let l = [10,20,30,40];
+l.forEach((value,index)=>{
+    console.log(value,index)
+});
+
+console.log("WElcome");
+console.log("Hii")
+
+console.log(addtocart());
+console.log(a);
+console.log(changeQ());
+
+
+
